da76e1d3808d391b2b45c466a7f50d361a2c77ad
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2020-08-13 16:18:09 +0600

    Removed Login option. Changed variable name movie to query.

--- main.py
+++ main.py
@@ -9,41 +9,52 @@ def getAuthToken(header: dict) -> dict:
     Return a dictonary containing a authorization token
     '''
     token = {}
-    url = 'https://www.opensubtitles.com/api/v1/login'
-    data = requests.post(url=url, data=header)
-    token['Authorization'] = json.loads(data.text)['token']
+    try:
+        url = 'https://www.opensubtitles.com/api/v1/login'
+        data = requests.post(url=url, data=header)
+        token['Authorization'] = json.loads(data.text)['token']
+    except ConnectionError:
+        print('No internet connection!\nTry again.')
     return token
 
 
-def findMovieSubIdByName(movie: dict, token: dict) -> dict:
+def findSubByName(query: dict, token: dict) -> dict:
     '''
     Return a dictonary containing subtitle file_id
     '''
     file_id = {}
     sub_id_list = []
     count = 1
+    
+    try:
+        url = 'https://www.opensubtitles.com/api/v1/find'
+        data = requests.get(url=url, params=query, headers=token)
+        json_data = json.loads(data.text)
+        consol_message = (
+            '''\n--- Choose your subtitle file from the search result ---'''
+        )
+        print(consol_message)
 
-    url = 'https://www.opensubtitles.com/api/v1/find'
-    data = requests.get(url=url, params=movie, headers=token)
-    json_data = json.loads(data.text)
-    consol_message = (
-        '''\n--- Choose your subtitle file from the search result ---'''
-    )
-    print(consol_message)
-
-    for movie_res in json_data['data']:
-        if count == 6:
-            break
-        movie_title = movie_res['attributes']['release']
-        sub_id = movie_res['attributes']['files'][0]['id']
-        print(f'\n{count}. Movie title: {movie_title}')
-        sub_id_list.append([sub_id, movie_title])
-        count += 1
-    if sub_id_list.__len__() == 0:
+        for movie_res in json_data['data']:
+            if count == 6:
+                break
+            movie_title = movie_res['attributes']['release']
+            sub_id = movie_res['attributes']['files'][0]['id']
+            print(f'\n{count}. Movie title: {movie_title}')
+            sub_id_list.append([sub_id, movie_title])
+            count += 1
+        if sub_id_list.__len__() == 0:
+            return {}
+        usr_option = int(input('\n=> '))
+        file_id = {'file_id': sub_id_list[usr_option-1]
+                [0], 'file_name': sub_id_list[usr_option-1][1], 'sub_format': 'str'}
+    except ConnectionError:
+        print('No internet connection!')
+    except IndexError:
+        print('Invalid input.')
+        findSubByName(query, token)
+    except ValueError:
         return {}
-    usr_option = int(input('\n=> '))
-    file_id = {'file_id': sub_id_list[usr_option-1]
-               [0], 'file_name': sub_id_list[usr_option-1][1], 'sub_format': 'str'}
     return file_id
 
 
@@ -56,20 +67,6 @@ def getSubLink(sub_file_id: dict, token: dict) -> dict:
     sub_link = json.loads(data.text)
     return sub_link
 
-
-def showUserData(token: dict):
-    '''
-    Return a dict object contains user data
-    '''
-    url = 'https://www.opensubtitles.com/api/v1/infos/user'
-    data = requests.get(url=url, headers=token)
-    data = json.loads(data.text)
-    message = (
-        '''\nUser ID: {0}\nlimit: {1}\nRemaining: {2}'''
-    ).format(data['data']['user_id'], data['data']['allowed_downloads'],
-             data['data']['remaining_downloads'])
-    print(message)
-
 def main():
     token = {}
 
@@ -81,43 +78,43 @@ def main():
         *        Choose an Option       *
         *********************************
         
-        1) Login/View Account
-        2) Movie Subtitle
-        3) Tv Series Subtitle
-        4) Exit
+        1) Search Subtitle
+        2) Exit
         '''
         )
         print(message)
-        option = int(input('=> '))
+        try:
+            option = int(input('=> '))
 
-        if option == 1 and len(token) == 0:
-            username = str(input('Username: '))
-            passowrd = getpass.getpass('Password: ')
-            header_ = {'username': username, 'password': passowrd}
-            token = getAuthToken(header=header_)
-            print('\nLogin successful...')
+            if option == 1:
+                header_ = {'username': 'rakibulraki10', 'password': 'D76GmNygKMbD8h.'}
+                token = getAuthToken(header=header_)
 
-        elif option == 2:
-            query = str(input('\nEnter a movie title: '))
-            movie_title = {'languages': 'en', 'query': query, 'type': 'movie'}
-            file_id = findMovieSubIdByName(movie_title, token)
-            link = getSubLink(file_id, token)
-            sub = requests.get(url=link['link'])
-            save_path = 'C:\\Users\\' + \
-                str(os.getlogin()) + '\\Downloads\\' + link['fname']
-            with open(save_path, 'w') as file:
-                file.write(str(sub.text))
-            print(f'\nFile saved. Location: {save_path}\n')
+                query = str(input('\nMovie/TV series title: '))
+                movie_title = {'languages': 'en', 'query': query, 'type': 'all'}
+                file_id = findSubByName(movie_title, token)
+                
+                if len(file_id) == 0:
+                    continue
+                
+                link = getSubLink(file_id, token)
+                sub = requests.get(url=link['link'])
+                save_path = 'C:\\Users\\' + \
+                    str(os.getlogin()) + '\\Downloads\\' + link['fname']
+                with open(save_path, 'w') as file:
+                    file.write(str(sub.text))
+                print(f'\nFile saved. Location: {save_path}\n')
 
-        elif option == 1 and len(token) > 0:
-            showUserData(token)
-
-        elif option == 4:
-            print('Exit Successfully...')
-            break
-
-        else:
-            print('Invalid option!')
+            elif option == 2:
+                print('Exit Successfully...')
+                break
+            else:
+                print('Invalid option!')
+        
+        except ValueError:
+            print('Invalid input.')
+        except Exception as ex:
+            print('Error!')
 
 
 if __name__ == '__main__':
